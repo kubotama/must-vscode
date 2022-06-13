6db62c0ce9b2e7650e69075afd2430611da421ee
kubotama <16697573+kubotama@users.noreply.github.com>
2022-06-14 00:12:08 +0900

    refact: Removed Markdown from function name.
    Prepare to change settings to read
    per-language settings from settings.json

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,49 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import axios from "axios";
 
-import { urlToMarkdownLink } from './markdown';
+import { JSDOM } from "jsdom";
+
+import * as vscode from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export const activate = (context: vscode.ExtensionContext) => {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	// console.log('Congratulations, your extension "must-vscode" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('must-vscode.urlToMarkdownLink', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-    const editor = vscode.window.activeTextEditor;
-    if (editor) {
-      const selection = editor.selection;
-      const document = editor.document;
-      const text = document.getText(selection);
-      if (text) {
-        urlToMarkdownLink(text).then((link) => {
-          editor.edit(editBuilder => {
-            editBuilder.replace(selection, link);
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "must-vscode.urlToLink",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        const selection = editor.selection;
+        const document = editor.document;
+        const text = document.getText(selection);
+        if (text) {
+          urlToLink(text).then((link) => {
+            editor.edit((editBuilder) => {
+              editBuilder.replace(selection, link);
+            });
           });
-        });
+        }
       }
     }
-	});
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 };
 
 // this method is called when your extension is deactivated
 export const deactivate = () => {};
+
+export const urlToLink = async (url: string): Promise<string> => {
+  try {
+    const response = await axios.get(url);
+    const dom = new JSDOM(response.data);
+    const title = dom.window.document.title;
+    return `[${title}](${url})`;
+  } catch (error) {
+    return url;
+  }
+};
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,30 +1,37 @@
-import * as assert from 'assert';
+import * as assert from "assert";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as vscode from "vscode";
 
-import { urlToMarkdownLink } from '../../markdown';
+import { urlToLink } from "../../extension";
 
-suite('Markdown Link', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite("Markdown Link", () => {
+  vscode.window.showInformationMessage("Start all tests.");
 
-	test('Google', (done) => {
-    urlToMarkdownLink('https://www.google.com').then((link) => {
-      assert.equal(link, '[Google](https://www.google.com)');
+  test("Google", (done) => {
+    urlToLink("https://www.google.com").then((link) => {
+      assert.equal(link, "[Google](https://www.google.com)");
     });
     done();
   });
-  test('GitHub', (done) => {
-    urlToMarkdownLink('https://www.github.com').then((link) => {
-          assert.equal(link, '[GitHub: Where the world builds software · GitHub](https://www.github.com)');
+
+  test("GitHub", (done) => {
+    urlToLink("https://www.github.com").then((link) => {
+      assert.equal(
+        link,
+        "[GitHub: Where the world builds software · GitHub](https://www.github.com)"
+      );
     });
     done();
   });
-  test('GitHub repository', (done) => {
-     urlToMarkdownLink('https://github.com/kubotama/must-vscode').then((link) => {
-       assert.equal(link, '[GitHub - kubotama/must-vscode: MarkUp support tool by netlify](https://github.com/kubotama/must-vscode)');
+
+  test("GitHub repository", (done) => {
+    urlToLink("https://github.com/kubotama/must-vscode").then((link) => {
+      assert.equal(
+        link,
+        "[GitHub - kubotama/must-vscode: MarkUp support tool by netlify](https://github.com/kubotama/must-vscode)"
+      );
     });
     done();
   });
