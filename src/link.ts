a0aab3394e2e000db9853a79c8277f0c599d4fbc
kubotama <16697573+kubotama@users.noreply.github.com>
2022-06-14 20:44:34 +0900

    Corrected to the correct function call.

--- src/link.ts
+++ src/link.ts
@@ -4,9 +4,7 @@ import { JSDOM } from "jsdom";
 
 export const urlToLink = async (url: string) => {
   try {
-    const response = await axios.get(url);
-    const dom = new JSDOM(response.data);
-    const title = dom.window.document.title;
+    const title = await getTitle(url);
     return `[${title}](${url})`;
   } catch (error) {
     return url;
