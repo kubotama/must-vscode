8fab956cd8c623baf208621ade12b501ef0aabef
kubotama <16697573+kubotama@users.noreply.github.com>
2022-06-15 10:46:47 +0900

    replace test suite from mocha to jest

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,30 +1,29 @@
-import * as assert from "assert";
+// import * as assert from "assert";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from "vscode";
+// import * as vscode from "vscode";
 
 import { getTitle } from "../../link";
 
-suite("Markdown Link", () => {
-  vscode.window.showInformationMessage("Start all tests.");
+describe("Markdown Link", () => {
+  // vscode.window.showInformationMessage("Start all tests.");
 
   test("Google", () => {
     return getTitle("https://www.google.com").then((link) => {
-      assert.equal(link, "Google");
+      expect(link).toEqual("Google");
     });
   });
 
   test("GitHub", () => {
     return getTitle("https://www.github.com").then((link) => {
-      assert.equal(link, "GitHub: Where the world builds software · GitHub");
+      expect(link).toEqual("GitHub: Where the world builds software · GitHub");
     });
   });
 
   test("GitHub repository", () => {
     return getTitle("https://github.com/kubotama/must-vscode").then((link) => {
-      assert.equal(
-        link,
+      expect(link).toEqual(
         "GitHub - kubotama/must-vscode: markup language support tool of Visual Studio Code extension"
       );
     });
