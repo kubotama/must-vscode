399c4cf6387638be35a725e23a09fdd2aa0b5f23
kubotama <16697573+kubotama@users.noreply.github.com>
2022-06-19 22:24:12 +0900

    Added link format reading.
    To define link formats in settings.json for each file language

--- src/extension.ts
+++ src/extension.ts
@@ -45,12 +45,12 @@ const replaceSelection = (linkSet: LinkSet) => {
     const selection = editor.selection;
     const selectedText = editor.document.getText(selection);
     if (selectedText) {
-      const title = linkSet.title;
-      const url = linkSet.url;
-      const linkFormat = `[${title}](${url})`;
-      editor.edit((editBuilder) => {
-        editBuilder.replace(selection, linkFormat);
-      });
+      const linkFormat = getLinkFormat(editor, linkSet);
+      if (linkFormat) {
+        editor.edit((editBuilder) => {
+          editBuilder.replace(selection, linkFormat);
+        });
+      }
     }
   }
 };
@@ -63,4 +63,34 @@ export const getTitlePatterns: () => TitlePattern[] = () => {
     return titlePatterns;
   }
   return [];
+};
+
+const getLinkFormat: (
+  editor: vscode.TextEditor,
+  linkSet: LinkSet
+) => string | undefined = (editor, linkSet) => {
+  const languageId = editor.document.languageId;
+  if (!languageId) {
+    return undefined;
+  }
+
+  const config = vscode.workspace.getConfiguration("must-vscode");
+  const linkFormats: { languageId: string; format: string }[] | undefined =
+    config.get("linkFormats");
+
+  if (linkFormats === undefined) {
+    vscode.window.showErrorMessage("No link format found");
+    return undefined;
+  }
+
+  for (const linkFormat of linkFormats) {
+    if (linkFormat.languageId === languageId) {
+      return linkFormat.format
+        .replace("${title}", linkSet.title)
+        .replace("${url}", linkSet.url);
+    }
+  }
+
+  vscode.window.showErrorMessage("No languages for link format found");
+  return undefined;
 };
